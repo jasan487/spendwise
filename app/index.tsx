bb864a8f8e320f3d7d9ef3737bbc70a8087a85f8
{name} <you@example.com>
2026-04-25 17:34:37 +0530

    feat: update dependencies and add Tailwind CSS configuration
    - Added @clerk/expo, babel-preset-expo, expo-secure-store, nativewind, and prettier-plugin-tailwindcss to dependencies.
    - Moved react-native-worklets to the correct position in dependencies.
    - Added tailwind.config.js for Nativewind integration.
    - Removed reset-project.js script as it is no longer needed.
    - Updated tsconfig.json to include nativewind-env.d.ts.

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="w-full h-screen bg-black">
+      <Text className="text-white">Hello World!</Text>
+    </View>
+  );
+}
